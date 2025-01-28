4cdf2fa3ee48895dc21f3963c5748fbb5905a2b8
toheed-pluginfy <toheed@pluginfy.com>
2025-01-28 19:20:49 +0500

    update git

---
+++ src/app/(dashboard)/student/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StudentPage = () => {
+    return (
+        <div>Student page</div>
+    );
+};
+
+export default StudentPage;
---
+++ src/app/(dashboard)/teacher/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TeacherPage = () => {
+    return (
+        <div>Teacher page
+            
+        </div>
+    );
+};
+
+export default TeacherPage;
